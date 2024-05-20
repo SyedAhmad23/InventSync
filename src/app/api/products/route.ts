76adc004042cfa9265e4beef932635739f6e9ff9
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-05-20 19:05:16 +0500

    done with get all products api

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,6 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import Product from "@/models/Product";
 import connectToDatabase from "@/app/lib/db";
 
+export async function GET() {
+  await connectToDatabase();
+
+  const products = await Product.find();
+
+  if (!products || products.length === 0) {
+    return NextResponse.json({ message: "No products found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ products });
+}
+
 export async function POST(req: NextRequest) {
   await connectToDatabase();
   const { name, category, description, image, quantity, price } =
