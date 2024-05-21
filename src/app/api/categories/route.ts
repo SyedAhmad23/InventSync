e552d417315ce113e487df03d1b0a3c1fa97a386
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-05-21 13:58:53 +0500

    Categories APIs added

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDatabase from "@/app/lib/db";
+import Category from "@/models/Category";
+
+export async function GET() {
+  await connectToDatabase();
+
+  const categories = await Category.find();
+
+  if (!categories || categories.length === 0) {
+    return NextResponse.json(
+      { message: "No categories found" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ categories });
+}
+
+export async function POST(req: NextRequest) {
+  await connectToDatabase();
+  const { name, description, type } = await req.json();
+
+  const newCategory = new Category({
+    name,
+    description,
+    type,
+  });
+
+  await newCategory.save();
+
+  return NextResponse.json(
+    { message: "Category created successfully", category: newCategory },
+    { status: 201 }
+  );
+}
