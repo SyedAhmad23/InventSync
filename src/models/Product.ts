63ac152dea5301939515fb6de738568b3e0680c2
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-05-22 12:40:06 +0500

    category ref added into the products

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,24 +1,17 @@
-import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
 
-interface Product {
-  name: string;
-  category: string;
-  description: string;
-  image: string;
-  quantity: number;
-  price: number;
-}
+const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
-const productSchema = new mongoose.Schema<Product>({
-  name: { type: String, required: true },
-  category: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
-});
-
-const Product =
-  mongoose.models.Product || mongoose.model<Product>("Product", productSchema);
+const Product = models.Product || model("Product", ProductSchema);
 
 export default Product;
