e9637d7eb6750a68b9ae1af869f8621276fac29f
send-2-irfan <Irfanullah.bses19@iba-suk.edu.pk>
2024-05-27 11:36:30 +0500

    done with get-product api

---
+++ src/app/api/invoices/get-product/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDatabase from "@/app/lib/db";
+import Product from "@/models/Product";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = params;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
+    const { _id, quantity, unitCode, sellPrice } = product;
+
+    const productDetails = {
+      id: _id,
+      avialable_quantity: quantity,
+      unitCode,
+      sellPrice,
+    };
+
+    return NextResponse.json(productDetails);
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return NextResponse.json(
+      { error: "Error fetching product" },
+      { status: 500 }
+    );
+  }
+}
